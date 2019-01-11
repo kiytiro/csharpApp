8ac19db94b1923812aa431d36c0699bb02cb1eef
kiytiro <akiytiro@harris.com>
2019-01-11 14:29:27 -0500

    Added Program.cs file

---
+++ src/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static object Passthrough(object obj)
+        {
+            return obj;
+        }
+    }
+}
